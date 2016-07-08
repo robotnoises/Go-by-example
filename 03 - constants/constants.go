318d7c79f546de35ae714a535e59a0e483a350de
nicholsd <nicholsd@opentext.com>
2016-07-08 16:50:31 -0400

    Add constants.go

---
+++ 03 - constants/constants.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const s string = "constant"
+
+func main() {
+
+	const n = 500000000
+	const d = 3e20 / n
+
+	fmt.Println(s)
+	fmt.Println(d)           // constants perform arithmetic with arbitrary precision
+	fmt.Println(int64(d))    // expllicit cast
+	fmt.Println(math.Sin(n)) // implicit type via func (also assignment)
+}
