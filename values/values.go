b8edfd2ee97cf621842c5dd734440bea19c4ed30
nicholsd <nicholsd@opentext.com>
2016-07-08 16:27:20 -0400

    Add values.go

---
+++ values/values.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + "lang")        // strings
+	fmt.Println("1+1 =", 1+1)         // integers
+	fmt.Println("7.0/3.0 =", 7.0/3.0) // floats
+	fmt.Println(true && false)        // booleans
+	fmt.Println(true || false)        // booleans
+	fmt.Println(!true)                // booleans
+}
